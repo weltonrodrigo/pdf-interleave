99e37818388ca94b04fc20b9e1e26f79af4a4ffe
Welton Rodrigo <rodrigo@familianascimento.org>
2012-04-27 17:07:32 -0400

    opção de reverter o segundo arquivo

--- bin/pdfreorder.pl
+++ bin/pdfreorder.pl
@@ -23,12 +23,20 @@ use strict;
 use warnings;
 use PDF::API2;
 use Data::Dumper;
+use Getopt::Long;
+
+my $oddfile;
+my $evenfile;
+my $revert = 1;
+
+GetOptions( "i=s" => \$oddfile, 
+			"p=s" => \$evenfile, 
+			"r"   => \$revert, 
+		);
 
 die "$0: São precisos dois arquivos,  primeiro as ímpares, depois as pares\n"
-	unless @ARGV == 2;
+	unless @ARGV >= 2;
 
-my $oddfile  = shift;
-my $evenfile = shift;
 
 my $new  = PDF::API2->new();
 my $odd  = PDF::API2->open($oddfile);
@@ -40,9 +48,15 @@ die "$0: Arquivos $evenfile $oddfile contém número ímpar de páginas\n"
 	unless $total % 2 == 0;
 
 my $i = 1;
-while ( $i =< $total / 2 ) {
+while ( $i <= $total / 2 ) {
     $new->importpage( $odd, $i );
-    $new->importpage( $even, $i );
+    if ( defined $revert ) {
+		# Mesclar o segundo arquivo na ordem reversa.
+        $new->importpage( $even, $even->pages() + 1 - $i );
+    }
+    else {
+        $new->importpage( $even, $i );
+    }
     $i++;
 }
 
