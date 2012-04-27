38097be70730fbbe77124051a6b060c88d2fcb38
Welton Rodrigo <rodrigo@familianascimento.org>
2012-04-27 17:21:45 -0400

    keep last

--- bin/pdfreorder.pl
+++ bin/pdfreorder.pl
@@ -27,12 +27,15 @@ use Getopt::Long;
 
 my $oddfile;
 my $evenfile;
-my $revert = 1;
+my $norevert;
+my $keeplast;
 
-GetOptions( "i=s" => \$oddfile, 
-			"p=s" => \$evenfile, 
-			"r"   => \$revert, 
-		);
+GetOptions(
+    "i=s" => \$oddfile,
+    "p=s" => \$evenfile,
+    "r"   => \$norevert,
+    "l"   => \$keeplast,
+);
 
 die "$0: São precisos dois arquivos,  primeiro as ímpares, depois as pares\n"
 	unless @ARGV >= 2;
@@ -50,11 +53,14 @@ die "$0: Arquivos $evenfile $oddfile contém número ímpar de páginas\n"
 my $i = 1;
 while ( $i <= $total / 2 ) {
     $new->importpage( $odd, $i );
-    if ( defined $revert ) {
+    if ( defined $norevert ) {
+		# Descartar a última página do conjunto par (normalmente em branco).
+		next if not defined $keeplast and $even->pages() - $i == 0;
 		# Mesclar o segundo arquivo na ordem reversa.
         $new->importpage( $even, $even->pages() + 1 - $i );
     }
     else {
+		next if not defined $keeplast and $even->pages() == $i;
         $new->importpage( $even, $i );
     }
     $i++;
