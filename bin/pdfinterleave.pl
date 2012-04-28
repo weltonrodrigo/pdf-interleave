161fba644529f2bf423f9d6a2b541fb36158b81e
Rodrigo Torres <rodrigo@familianascimento.org>
2012-04-27 22:41:48 -0400

    English speaking

--- bin/pdfinterleave.pl
+++ bin/pdfinterleave.pl
@@ -5,14 +5,13 @@
 #
 #        USAGE: ./pdfinterleave.pl oddpages.pdf evenpages.pdf
 #
-#  DESCRIPTION: Reorganiza arquivos PDF frente e verso que foram escaneados um 
-#  		lado de cada vez.
+#  DESCRIPTION: Interleaves the pages of two pdf files.
 #
 #      OPTIONS: ---
 # REQUIREMENTS: ---
 #         BUGS: ---
 #        NOTES: ---
-#       AUTHOR: WELTON RODRIGO TORRES NASCIMENTO (rodrigo@familianascimento.org), 
+#       AUTHOR: WELTON RODRIGO TORRES NASCIMENTO (rodrigo@familianascimento.org)
 # ORGANIZATION: 
 #      VERSION: 1.0
 #      CREATED: 27-04-2012 14:22:51
@@ -41,16 +40,17 @@ my $even = PDF::API2->open(shift);
 
 my $total = $odd->pages() + $even->pages();
 
-die "$0: Arquivos $evenfile $oddfile contém número ímpar de páginas\n"
+die "$0: Files\n$evenfile\n$oddfile\ncontains a different number of pages.\n"
 	unless $total % 2 == 0;
 
 my $i = 1;
 while ( $i <= $total / 2 ) {
     $new->importpage( $odd, $i );
     if ( not defined $norevert ) {
-		# Descartar a última página do conjunto par (normalmente em branco).
+		# Discard last page of the even file. (usually blank).
 		last if defined $discardlast and $even->pages() - $i == 0;
-		# Mesclar o segundo arquivo na ordem reversa.
+
+		# Interleaves second file in reverse order. (facilitate scanning).
         $new->importpage( $even, $even->pages() + 1 - $i );
     }
     else {
